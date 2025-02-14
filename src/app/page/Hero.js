fd1ebae2ed4626b1c60c1dbc6ec7ce92e4fc9128
Your Name <adegokejulius46@gmail.com>
2025-02-13 20:47:55 -0800

    corrected hero page

--- src/app/page/Hero.js
+++ src/app/page/Hero.js
@@ -3,6 +3,17 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
+
+
+
+
+
+
+<div>
+
+
+
+
     <div className=' hidden md:block  hero ' id='hero'>
       <div className='flex     flex-row flex  justify-between max-w-6xl  mx-auto pt-12    '>
       <div className='h-[60vh] w-[400px] ml-[40px] mt-[20px]'>
@@ -35,6 +46,65 @@ LEARN MORE
       </div>
       </div>
     </div>
+
+    <div className='md:hidden'>
+
+      <div>
+        <div className='flex flex-col   md:flex-row flex   gap-[60px] px-4  mx-auto  max-w-5xl mt-[20px]  items-center py-4   mt-[90px]' >
+                    <div className=''>
+                        <Image  src={"/arme.png"}  className=''  width={400} height={400} objectFit='contain'/>
+                        {/* <Image  src={"/arm.svg"}  className='md:hidden'  width={100} height={100} objectFit='contain'/> */}
+                    </div>
+                <div className='max-w-5xl flex flex-col gap-2  '>
+                    <div className='text-black flex-col  justify-between items-center   '  >
+                        <div className='flex items-center justify-center '>
+        
+                        <p className='text-[22px] self-center  font-[800]   uppercase  text-black '>
+                        Chief Olaleye Olanipekun  <br/>   <span className='px-[70px]'> Amstrong    </span>              </p>
+                  
+                        </div>
+                    
+                        </div>
+                </div>
+            </div>
+
+      </div>
+
+
+
+
+<div className='hero1  mt-[60px]'>
+  <div className=' flex flex-col gap-8  py-12 max-w-5xl justify-center items-center mx-auto px-[80px]'>
+      <h1 className='px-3 font-[600] text-[22px] max-w-3xl text-center'>Empowering Businesses, 
+      Inspiring Leaders</h1>
+      <p className='text-justify'>A visionary leader, accomplished entrepreneur, and the Agba-Akin of Okin-Apa—building legacies in oil & gas, real estate, media, and beyond.</p>
+      <p className='text-start text-pretty  max-w-3xl'> 30+ Years in Business (Oil & Gas, Real Estate, Farming, Media, Fashion)
+MBA in Marketing from LAUTECH
+Public Speaker & Author on Business Growth & Leadership
+Fellow of the Nigeria Lions Club Foundation
+Passionate About Fairness & Influence</p>
+  </div>
+
+
+
+</div>
+
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
   )
 }
 
